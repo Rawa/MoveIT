9d55d1b8822d17d5b7e30cb6985b52fb2502bf3f
stroem <jonathan.strom@gmail.com>
2015-05-16 17:56:48 +0200

    White gubbe

--- app/src/main/java/gbgsh/moveit/MainActivity.java
+++ app/src/main/java/gbgsh/moveit/MainActivity.java
@@ -199,7 +199,7 @@ public class MainActivity extends Activity implements Runnable {
         paint.setColor(getColorByIntesity(happyness));
         canvas.drawCircle((23+x)*scale, (23+y)*scale, 20*scale, paint);
 
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.WHITE);
         canvas.drawCircle((20+x+10)*scale, (15+y)*scale, 4*scale, paint); //Left eye
         canvas.drawCircle((20+x-5)*scale, (15+y)*scale, 4*scale, paint); //Right eye
         //canvas.drawCircle(31, 15, 3, paint); //Right eye
