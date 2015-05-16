4439c62d814e9bd6bbb38ceb4b6bbc2aab4b9719
Erik Axelsson <erikaxelsson1@gmail.com>
2015-05-16 12:48:21 +0200

    merge

---
+++ app/src/main/java/gbgsh/moveit/UserSettingActivity.java
@@ -0,0 +1,16 @@
+package gbgsh.moveit;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.preference.PreferenceActivity;
+        import android.view.Menu;
+        import android.view.MenuItem;
+
+public class UserSettingActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  addPreferencesFromResource(R.xml.prefs);
+    }
+
+}
